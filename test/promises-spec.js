0de22cc9eb935382b2e107aafe6973035b8820f5
Joe Heck <heckj@mac.com>
2014-07-15 15:27:14 -0700

    basic promises with tests

--- test/promises-spec.js
+++ test/promises-spec.js
@@ -12,4 +12,26 @@
             expect(1).to.be.ok;
         });
     });
+
+    describe("basic promises", function() {
+        it("should return a promise", function() {
+            var myPromise = index.doSomething();
+            // myPromise should be defined
+            expect(myPromise).to.be.ok;
+            // and if it's a promise, then it should have a "then" on it
+            expect(myPromise.then).to.be.ok;
+        });
+        it("should resolve the promise", function(wrapit) {
+            var myPromise = index.doSomething();
+            expect(myPromise).to.be.ok;
+            myPromise.then(function(result) {
+                expect(result).to.equal("doSomething");
+                wrapit();
+            }, function(err) {
+                expect(err).to.be.undefined;
+                wrapit();
+            }).done();
+
+        });
+    });
 }(module.exports));
--- testcode.js
+++ testcode.js
@@ -1,5 +1,17 @@
 /* jslint node: true */
 (function(index){
     'use strict';
-    index.doSomething = function() {}
+    // the triumvirate of interesting libraries I want to show examples on using...
+    var Q = require('q'),
+        _ = require('lodash'),
+        async = require('async');
+
+    index.doSomething = function() {
+        var deferred = Q.defer();
+        setTimeout(function() {
+            deferred.resolve("doSomething");
+        }, 100);
+        return deferred.promise;
+    };
+
 }(module.exports));
