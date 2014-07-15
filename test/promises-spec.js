8cb45f8c7e6fae28160fa29c4debea0a37f51215
Joe Heck <heckj@mac.com>
2014-07-15 13:59:40 -0700

    stubbed in tests

---
+++ test/promises-spec.js
@@ -0,0 +1,15 @@
+// Created by heckj on 7/15/14.
+/* jslint node: true */
+
+(function () {
+    'use strict';
+    require('./helper');
+
+    var index = require('../index.js');
+
+    describe("Initial test", function() {
+        it("should always pass", function() {
+            expect(1).to.be.ok;
+        });
+    });
+}(module.exports));
